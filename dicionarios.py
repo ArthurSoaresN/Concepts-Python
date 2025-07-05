7a41b555a2a733d470aab5da13581245eac3b344
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-05 16:51:41 -0300

    dict note 1

---
+++ dicionarios.py
@@ -0,0 +1,10 @@
+# Conjunto não ordenado de pares
+# Chave: Valor
+# As chaves são imutáveis
+
+# Dicionários são delimitados por chaves e contem uma lista de pares separados por vigulas
+
+pessoa = {
+    "nome": "Jose",
+    "idade": 20
+}
