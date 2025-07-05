4481b0d23e222136e0bb0a0685ec8ad5d0b65ab1
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-05 17:02:56 -0300

    note 4 dict

--- dicionarios.py
+++ dicionarios.py
@@ -39,3 +39,25 @@ print(pessoa["nome"])
 pessoa["nome"] = "Marcos"
 
 print(pessoa)
+
+# Dicionarios aninhados
+# Podem armazenar qualquer tipo de objeto python como valor, desde que chave seja imutavel
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "idade": 20},
+    "giovana@gmail.com": {"nome": "Giovana", "idade": 22},
+    "jorge@gmail.com": {"nome": "Jorge", "idade": 25}, #virgula no final
+}
+
+print(type(contatos))
+
+def show_info(email: str):
+    if email in contatos:
+        info = contatos[email]
+        print(f"Informações para {email}:")
+        print(f"  Nome: {info['nome']}")
+        print(f"  Idade: {info['idade']}")
+    else:
+        print(f"Erro: O email '{email}' não foi encontrado nos contatos.")
+
+show_info("jorge@gmail.com")
