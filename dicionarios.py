fdb83e1e338938108aafddbe77e0f8a6ae14cf5d
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-05 16:57:12 -0300

    note 3 dict

--- dicionarios.py
+++ dicionarios.py
@@ -4,6 +4,18 @@
 
 # Dicionários são delimitados por chaves e contem uma lista de pares separados por vigulas
 
+import os
+
+def limpar_tela():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+#main
+limpar_tela()
+
 pessoa = {
     "nome": "Jose",
     "idade": 20
@@ -14,5 +26,16 @@ pessoa2 = dict(nome= "Carlos", idade = 22)
 
 pessoa["telefone"] = "7499658256"
 # Adcionando uma chave no dicionário pessoa
+
+print(pessoa)
+print(pessoa2)
+
+# Para acessar o conteudo de uma chave
+
+print(pessoa["nome"])
+
+# Sobre-escrevendo
+
+pessoa["nome"] = "Marcos"
 
 print(pessoa)
