4136a0db89930c4758753164ed58be48fb9caa0d
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-06 20:54:55 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,3 +10,8 @@ def limpar_tela():
 limpar_tela()
 
 #Função é um bloco de código identificado por um nome e pode receber parâmetros
+
+def exibir_mensagem(nome):
+    print(f"Ola {nome}!")
+
+exibir_mensagem("Jose")
