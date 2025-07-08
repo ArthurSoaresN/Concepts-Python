a9c6f7174b90743cde67835b2e0008c4b1f189b4
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 16:07:26 -0300

    Classe bicicleta

---
+++ bicicletaria/desafio_bicicletaria.py
@@ -0,0 +1,8 @@
+class Bicicleta:
+    def __init__(self, cor, modelo, ano, valor): #Atributos
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+caloi = Bicicleta("Preta", "Caloi", 2024, 1400)
