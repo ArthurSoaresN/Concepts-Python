c79fdee7e05e9ac587f87d019d3a1d9c650778c7
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 20:51:59 -0300

    Update encapsulamento.py

--- encapsulamento.py
+++ encapsulamento.py
@@ -4,3 +4,11 @@
 # Ele descreve a ideia de agrupar dados e os métodos que manipulam esses dados em uma unidade
 # Isso impõe restrições ao acesso direto as variáveis e métodos e pode evitar a modificação acidental de dados
 # Para evitar alterações acidentais, a variável de um objeto só pode ser alterado pelo método desse objeto
+
+# Exemplo:
+# Atributo privado
+# - saldo: float
+
+# Atributo não privado
+# + depositar
+# + sacar
