cff39a218917770b3ced071975f24d536d02433a
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 20:46:28 -0300

    Create encapsulamento.py

---
+++ encapsulamento.py
@@ -0,0 +1,6 @@
+# Encapsulamento
+
+# Proteção aos dados
+# Ele descreve a ideia de agrupar dados e os métodos que manipulam esses dados em uma unidade
+# Isso impõe restrições ao acesso direto as variáveis e métodos e pode evitar a modificação acidental de dados
+# Para evitar alterações acidentais, a variável de um objeto só pode ser alterado pelo método desse objeto
