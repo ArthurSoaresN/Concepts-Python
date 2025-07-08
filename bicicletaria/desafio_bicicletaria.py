b6710fba040d21849eadf4d2bf437d121ce80675
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 16:09:40 -0300

    Metodos

--- bicicletaria/desafio_bicicletaria.py
+++ bicicletaria/desafio_bicicletaria.py
@@ -5,4 +5,14 @@ class Bicicleta:
         self.ano = ano
         self.valor = valor
 
+    def buzinar(self):  #Métodos
+        print(f"Bicicleta {self.modelo}: Buzinou")
+
+    def parar(self):
+        print(f"Bicicleta {self.modelo}: Parou")
+
+
+
 caloi = Bicicleta("Preta", "Caloi", 2024, 1400)
+
+caloi.buzinar()
