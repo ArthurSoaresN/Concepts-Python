598b13897c47ac596b6a41120e68449a6953adcc
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 20:21:09 -0300

    Update heranca_simples.py

--- heranca_simples.py
+++ heranca_simples.py
@@ -8,6 +8,9 @@ class veiculo:
     def ligar_motor(self):
         print("Motor Ligado")
         return True
+    
+    def __str__(self):
+        pass
 
 class motocicleta(veiculo):
     pass
@@ -16,10 +19,19 @@ class carro(veiculo):
     pass
 
 class caminhao(veiculo):
-    pass
+    def __init__(self, cor, placa, numero_rodas, carregado): #Sobrescrevendo
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Esta carregado' if self.carregado else 'Não esta carregado'}")
 
 moto = motocicleta("Preto", "ABC-1234", 2)
 moto.ligar_motor()
 
 carro = carro("Branco", "CBA-4321", 4)
 carro.ligar_motor()
+
+caminhao = caminhao("Cinza", "LKK-9999", 8, True)
+caminhao.ligar_motor()
+caminhao.esta_carregado()
