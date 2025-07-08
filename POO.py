80ee2a17bc333056e984d29bfdba079007deb208
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 14:46:11 -0300

    Create POO.py

---
+++ POO.py
@@ -0,0 +1,8 @@
+# Paradigma POO
+
+# Forma de solucionar os problemas através do código
+# Abstração de problemas em objetos do mundo real, facilitando o entendimento do código
+# e tornando mais fácil de modular e extensível.
+
+# Conceitos Chaves:
+# Classes e Objetos
