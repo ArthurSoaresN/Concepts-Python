c86a10bfd31333221705005f4a64a29267f520d8
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 15:08:56 -0300

    Classe cachorro

--- POO.py
+++ POO.py
@@ -12,6 +12,7 @@
 # Define as características e comportamentos de um objeto, porém não conseguimos usá-las diretamente. 
 # Já os objetos podemos usá-los e eles possuem as características e comportamentos que foram definidos pela classe.
 
+#Classe
 class Cachorro:
     def __init__(self, nome, cor, acordado = True): # Construtor
         self.nome = nome
