7be23302a910f5d1a63f6fbdd41038f291a5030a
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-08 22:44:12 -0300

    Update propriedades.py

--- propriedades.py
+++ propriedades.py
@@ -3,3 +3,24 @@
 # com o property() do Python, voce pode criar atributos gerenciados em suas classes (propriedades)
 # sem alterar a API publica da classe
 
+class Foo:
+    def __init__ (self, x=None):
+        self._x = x
+    
+    @property # Propriedade
+    def x(self):
+        return self._x or 0
+
+    @x.setter
+    def x(self, valor):
+        self._x += valor
+
+    @x.deleter
+    def x(self):
+        self._x = -1
+    
+foo = Foo(10)
+print(foo.x)
+foo.x = 10
+print(foo.x)
+    
