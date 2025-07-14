8d7acc10a35ff3c9be9aa48f47c327b6febbaa79
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-14 20:01:08 -0300

    Update variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -3,3 +3,16 @@
 # Todos os objetos nascem com o mesmo numero de atributos de classe 
 # e de instancia são diferentes para cada objeto (cada objeto tem uma copia)
 # Ja os atributos de classe são compartilhados entre os objetos
+
+class Estudante:
+    escola = "DIO"
+
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+    def __str__(self)->str:
+        return f"{self.nome} - {self.matricula} - {self.escola}"
+    
+aluno1 = Estudante("Joao", "11")
+print(aluno1)
