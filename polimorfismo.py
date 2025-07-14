6bbc7f62c1941ac5b888ce6aaafc054e3a0a4005
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-14 19:49:57 -0300

    Update polimorfismo.py

---
+++ polimorfismo.py
@@ -0,0 +1,37 @@
+# Polimorfismo
+# 
+# Essa palavra significa ter muitas formas. Na programação
+# significa o mesmo nome de função mas assinaturas diferentes
+# sendo usado para tipos diferentes
+
+# Exemplo: len()
+# len("python")
+# len([10,20,30])
+
+# Polimorfismo com herança
+
+# Na herança, a classe filha herda os métodos da classe pai
+# No entando, é possível modificar um método em uma classe filha
+# herdada da classe pai. Isse é útil nos casos em que o método
+# da classe pai não se encaixa perfeitamente na classe filha
+
+class Passaro:
+    def voar(self):
+        print("Voando...")
+    
+class Pardal(Passaro):
+    def voar(self):
+        super().voar()
+
+class Avestruz(Passaro):
+    def voar(self):
+        print("Avestruz nao pode voar")
+
+def plano_voo(obj):
+    obj.voar()
+
+p1 = Pardal()
+p2 = Avestruz()
+
+plano_voo(p1)
+plano_voo(p2)
