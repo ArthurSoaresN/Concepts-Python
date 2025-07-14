e030668773a351aa747865b3d8a5ed50ecb750a4
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-14 20:11:25 -0300

    Update metodos_de_classe.py

--- metodos_de_classe.py
+++ metodos_de_classe.py
@@ -1,3 +1,9 @@
 # Metodos de classe estão ligados a classe e não ao objeto.
 # Eles têm acesso ao estado da classe, pois recebem um parâmetro
 # que aponta para a classe e não para a instância do objeto.
+
+# Metodos estáticos não recebe um primeiro argumento
+# Ele tambem é um metodo vinculado a classe e não ao 
+# objeto da classe. Este método não pode acessar ou modificar o estado
+# da classe. Ele está presente em uma classe porque faz sentido
+# que o método esteja presente na classe
