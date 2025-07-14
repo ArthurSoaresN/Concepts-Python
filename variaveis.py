cd73ecdbd014898ad45d9e0914fc971de6c68005
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-14 19:57:15 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,5 @@
+# Atributos do objetos
+
+# Todos os objetos nascem com o mesmo numero de atributos de classe 
+# e de instancia são diferentes para cada objeto (cada objeto tem uma copia)
+# Ja os atributos de classe são compartilhados entre os objetos
