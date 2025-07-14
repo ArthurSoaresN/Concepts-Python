b5643f4f85a52c0296120e5b87174fd8acb4d51d
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-14 20:08:26 -0300

    Create metodos_de_classe.py

---
+++ metodos_de_classe.py
@@ -0,0 +1,3 @@
+# Metodos de classe estão ligados a classe e não ao objeto.
+# Eles têm acesso ao estado da classe, pois recebem um parâmetro
+# que aponta para a classe e não para a instância do objeto.
