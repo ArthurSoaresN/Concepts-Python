1fd447623b5172095b5da588556f6546e021ac70
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-14 20:19:38 -0300

    Update metodos_de_classe.py

--- metodos_de_classe.py
+++ metodos_de_classe.py
@@ -7,3 +7,31 @@
 # objeto da classe. Este método não pode acessar ou modificar o estado
 # da classe. Ele está presente em uma classe porque faz sentido
 # que o método esteja presente na classe
+
+# Metodo de classe pode acessar ou modificar o estado
+# graças ao primeiro argumento que se referencia
+
+# Geralmente usamos metodos estaticos para criar funções
+# utilitárias
+
+ANO_ATUAL = 2025
+
+class Pessoa:
+    def __init__(self, nome: None, idade: None):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def criar_por_data_nascimento(self, ano, dia ,mes, nome):
+        idade = ANO_ATUAL - ano
+        return Pessoa(nome, idade)
+    
+    @staticmethod
+    def maior_de_idade(idade):
+        return idade >= 18
+
+p1 = Pessoa.criar_por_data_nascimento(1994, 13, 5, "Carlos")
+print(p1.nome, p1.idade)
+
+print(Pessoa.maior_de_idade(18)) 
+print(Pessoa.maior_de_idade(12)) 
