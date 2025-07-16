8a0c61196ae4c603a11fbbc02727fa4db22ec70f
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-16 20:09:11 -0300

    Update decoradores.py

--- decoradores.py
+++ decoradores.py
@@ -45,3 +45,14 @@ def ola_mundo():
 ola_mundo = meu_decorador(ola_mundo) # Sem acucar sintatico
 ola_mundo()
 
+def duplicar(func):
+    def envelope(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+    return envelope
+
+@duplicar
+def aprender(tecnologia):
+    print(f"Estou aprendendo {tecnologia}")
+
+aprender("Python")
