e46c84668c7b658b290dcae58911eeeea3d507b5
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-16 23:29:34 -0300

    Update geradores.py

--- geradores.py
+++ geradores.py
@@ -3,3 +3,9 @@
 # São tipos especiais de iteradores, ao contrário das listas ou outros iteráveis
 # não armazenam todos os seus valores na memória
 # São definidos usando funções regulares, mas não returna valores usando "return" utilizam "yield"
+
+# Caracteristicas
+
+# Uma vez que um item gerado é consumido, ele é esquicido e não pode ser acessado de novo
+# O estado interno de um gerador é mantido entre chamadas
+# A execução de um gerador é pausado na declaração "yield" e retoma dai na proxima vez que for chamado
