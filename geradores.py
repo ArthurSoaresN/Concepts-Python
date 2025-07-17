ba43c9cf8eac248379b5be6bead32f2c17d4518f
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-16 23:27:40 -0300

    Create geradores.py

---
+++ geradores.py
@@ -0,0 +1,5 @@
+# Geradores
+
+# São tipos especiais de iteradores, ao contrário das listas ou outros iteráveis
+# não armazenam todos os seus valores na memória
+# São definidos usando funções regulares, mas não returna valores usando "return" utilizam "yield"
