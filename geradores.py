31504fe537fa1bb19f17ab963b10ddcb35024377
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-16 23:40:07 -0300

    Update geradores.py

--- geradores.py
+++ geradores.py
@@ -9,3 +9,19 @@
 # Uma vez que um item gerado é consumido, ele é esquicido e não pode ser acessado de novo
 # O estado interno de um gerador é mantido entre chamadas
 # A execução de um gerador é pausado na declaração "yield" e retoma dai na proxima vez que for chamado
+
+import requests
+
+def fetch_products(api_url, max_pages = 100):
+    page = 1
+    while page <= max_pages:
+        reponse = requests.get(f"{api_url}?page={page}")
+        data = reponse.json()
+        for product in data["products"]:
+            yield product
+        if 'next_page' not in data:
+            break
+        page += 1
+
+for product in fetch_products("link_api"):
+    print(product['name'])
