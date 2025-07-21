97ad9e83d39434465591796894c9e337df961159
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-21 01:12:59 -0300

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -5,3 +5,11 @@ import datetime
 d = datetime.date(2023, 7, 9) #ano, mes, dia
 print(d)
 print(datetime.datetime.today())
+
+# Manipulando datas com timedelta
+# Operação matematica com datas
+
+data = datetime.datetime(2025, 7, 21, 1, 30)
+data2 = data + datetime.timedelta(weeks=1)
+print(data)
+print(data2)
