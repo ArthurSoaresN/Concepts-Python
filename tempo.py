2aff7275df7ad47c77891d8d9827ca4157b985c6
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-21 01:22:32 -0300

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -13,3 +13,24 @@ data = datetime.datetime(2025, 7, 21, 1, 30)
 data2 = data + datetime.timedelta(weeks=1)
 print(data)
 print(data2)
+
+data_atual = datetime.datetime.now()
+tempo_estimado1 = 30
+tempo_estimado2 = 45
+tempo_estimado3 = 60   
+
+
+print(
+    f"Agora são {data_atual}, "
+    f"tempo estimado para entrega é de: {data_atual + datetime.timedelta(minutes=tempo_estimado1)}"
+)
+
+print(
+    f"Agora são {data_atual}, "
+    f"tempo estimado para entrega é de: {data_atual + datetime.timedelta(minutes=tempo_estimado2)}"
+)
+
+print(
+    f"Agora são {data_atual}, "
+    f"tempo estimado para entrega é de: {data_atual + datetime.timedelta(minutes=tempo_estimado3)}"
+)
