e7a146458935afd38267ad81013efdfb5d86904c
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-22 01:57:32 -0300

    Create arquivos.py

---
+++ arquivos.py
@@ -0,0 +1,8 @@
+# Formatos txt e csv (trocas de informação)
+
+# Os arquivos são essenciais por fornecer um meio de armazenar e recuperar dados
+# Através da manipulação de arquivos, podemos persistir os dados além da vida útil
+# de programas específicos
+
+# Um arquivo é um container no computador, onde as informações são armazenadas em formato digital
+# Em Python podemos manipular arquivos de texto e binários
