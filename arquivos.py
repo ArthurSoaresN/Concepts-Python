a57eb54b895866c4a59405d5003cd6cabc4049c5
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-22 02:10:30 -0300

    open() test

--- arquivos.py
+++ arquivos.py
@@ -6,3 +6,15 @@
 
 # Um arquivo é um container no computador, onde as informações são armazenadas em formato digital
 # Em Python podemos manipular arquivos de texto e binários
+
+# file = open('example.txt', 'r')
+# file = open('example.txt', 'w')
+# file = open('example.txt', 'a')
+
+# Método read, readline, readlines
+
+# -*- coding: utf-8 -*-
+
+arquivo = open("arq.txt", "r") # Caminho relativo
+print(arquivo.read())
+arquivo.close()
