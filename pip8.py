331bcbe90773ac2e3dd76da8c12a1b46402d80d5
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-24 22:31:05 -0300

    Update pip8.py

--- pip8.py
+++ pip8.py
@@ -1,1 +1,10 @@
 # Gerenciamento de pacotes
+
+# Pacotes são modulos que podem ser instalados e utilizados
+# nos programas python
+# Eles permitem que você utilize código que foi escrito por outras pessoas
+# economizando tempo e esforço
+
+# Pip é o gerenciador de pacotes do python, eles nos permite instalar
+# atualizar e remover pacotes
+# pypi.org -> baixar pacotes para usar no python
