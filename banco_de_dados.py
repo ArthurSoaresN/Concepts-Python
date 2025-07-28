5066ac05b847948aa4560fbac7cc31e9c3c965e3
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-28 02:22:37 -0300

    Update banco_de_dados.py

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -4,3 +4,10 @@
 # Tipos mais usados: relacional e não relacional
 # Relacional: Organizado em tabelas, com colunas e linhas com relação entre si
 # Exemplo: SQL Server, ORACLE DataBase, MySQL
+
+# Tabelas: São dados organizados logicamente em formato de linha e coluna
+#   ------------------------------------------
+#  | ID | NOME | SOBRENOME | EMAIL | CADASTRO |
+#   ------------------------------------------
+
+ 
