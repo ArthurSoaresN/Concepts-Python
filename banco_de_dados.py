8393592fa8ca51d68bc558657f55c84c5f006c6c
Arthur Soares <arthursoaressi@hotmail.com>
2025-07-28 01:53:27 -0300

    Update banco_de_dados.py

---
+++ banco_de_dados.py
@@ -0,0 +1,6 @@
+# Coleção organizada de informações estruturadas, armazenadas eletronicamente
+# em um sistema de computador
+
+# Tipos mais usados: relacional e não relacional
+# Relacional: Organizado em tabelas, com colunas e linhas com relação entre si
+# Exemplo: SQL Server, ORACLE DataBase, MySQL
