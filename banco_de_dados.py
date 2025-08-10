7f900a3b74afd3158c5ecbddc0ee06679a655b3a
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-10 13:47:04 -0300

    Update banco_de_dados.py

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -140,6 +140,9 @@
 # PSYCOPG -> Driver para trabalhar com banco de dados
 
 import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
 
 conexao = sqlite3.connect("clientes.db")
 print(conexao)
