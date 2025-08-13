3cc98a1bede018972c4f45af3b2c1ac49c95a56e
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-13 00:41:52 -0300

    O que é API?

--- desenvolvimento_web.py
+++ desenvolvimento_web.py
@@ -2,3 +2,8 @@
 # Abrange web design, programação web, gestão de bancos de dados e engenharia de servidores
 
 # Back end é a parte do site que o usuário não vê. Inclui servidor, aplicação e banco de dados
+
+# O que é uma API?
+# API ou Interface de Programação de Aplicações, é um conjunto de regras e definições
+# que permite que diferentes aplicações de software ou componentes de comuniquem entre si.
+# Funciona como um intermediário
