2191fd5bb9e3a6559749780f38d1faf4896dd9cf
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-13 00:37:47 -0300

    Update desenvolvimento_web.py

--- desenvolvimento_web.py
+++ desenvolvimento_web.py
@@ -1,2 +1,4 @@
 # Processo de criação de websites e aplicações para internet e intranet
 # Abrange web design, programação web, gestão de bancos de dados e engenharia de servidores
+
+# Back end é a parte do site que o usuário não vê. Inclui servidor, aplicação e banco de dados
